65b3faa330e8a9ac8c95638121a6dbf50f7bcbe5
victormartin <victormartin@ceroca.cat>
2025-03-11 17:19:48 +0100

    Comentarios y Valoraciones en recetas hecho
    Se añaden en los comentarios y en la base de datos

--- favoritos.js
+++ favoritos.js
@@ -1,7 +1,4 @@
-// Configuración de Supabase
-const SUPABASE_URL = "https://uonkcjrokwtgvimjxawm.supabase.co";
-const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvbmtjanJva3d0Z3ZpbWp4YXdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2ODIyNTgsImV4cCI6MjA1NDI1ODI1OH0.fTH7cyyYYQFi5HQc8y-JXAKSY0PL3P1FKy6LymfeTvU";
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_API_KEY);
+import { supabase } from "./supabaseConfig.js";
 
 async function agregarAFavoritos(recetaId) {
     try {
